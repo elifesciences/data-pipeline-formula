294ae83481e113a85f701931ec37520358461ad0
Luke <lsh-0@users.noreply.github.com>
2018-10-16 13:44:10 +1030

    process-zip-apply.sh script now removes it's input after processing. it's onerous to do this through nifi

--- salt/data-pipeline/scripts/process-zip-apply.sh
+++ salt/data-pipeline/scripts/process-zip-apply.sh
@@ -1,12 +1,14 @@
 #/bin/bash
-# ejp-XML-scraper, 
+# WARN: removes input
+# converts zip file to csv, applies changes in db, removes source zip
 
 set -e
 source_zip=$1
 source_basename=$(basename "$source_zip")
 data_dir="/ext/ejp-to-json-converter/data"
 
-test -e "$data_dir/$source_basename"
+actual_zip="$data_dir/$source_basename"
+test -e "$actual_zip"
 
 cd /opt/data-pipeline-ejp-to-json-converter
 docker-compose run --rm \
@@ -18,3 +20,5 @@ docker-compose run --rm \
 docker-compose run --rm \
     ejp-to-json-converter \
     python -m ejp_to_json_converter.apply_changes
+
+rm -f "$actual_zip"
