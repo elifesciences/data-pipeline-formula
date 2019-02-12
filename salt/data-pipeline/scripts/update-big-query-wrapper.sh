526d579ab6a2e35d8f394fe8a5f7bff10ba49140
Luke <lsh-0@users.noreply.github.com>
2019-02-12 16:00:28 +1030

    added optional env param to update-big-query-wrapper

--- salt/data-pipeline/scripts/update-big-query-wrapper.sh
+++ salt/data-pipeline/scripts/update-big-query-wrapper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # run as root
-set -e
+set -eu
+environment=${1:-{{ pillar.elife.env }}} # backwards compatibility: default to instance environment
 sudo -u elife --login /bin/bash << EOF
 cd /opt/data-pipeline-ejp-to-json-converter/
-DATA_PIPELINE_BQ_DATASET={{ pillar.elife.env }} ./update-big-query.sh
+DATA_PIPELINE_BQ_DATASET=$environment ./update-big-query.sh
 EOF
 #
 
